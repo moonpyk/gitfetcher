3352b7e6274c407564a4afc2bbd15512a2a5cb02
Clément Bourgeois <moonpyk@gmail.com>
2013-06-27 15:45:25 +0200

    Staring over with a NodeJS version.
    For now, a bit behind the Python version, except the "--add-project" option.

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from gitfetcher import main
-
-if __name__ == '__main__':
-    main()
-  
---
+++ test/util_test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var myTest = require('../lib/util');
+
+/*
+ ======== A Handy Little Nodeunit Reference ========
+ https://github.com/caolan/nodeunit
+
+ Test methods:
+ test.expect(numAssertions)
+ test.done()
+ Test assertions:
+ test.ok(value, [message])
+ test.equal(actual, expected, [message])
+ test.notEqual(actual, expected, [message])
+ test.deepEqual(actual, expected, [message])
+ test.notDeepEqual(actual, expected, [message])
+ test.strictEqual(actual, expected, [message])
+ test.notStrictEqual(actual, expected, [message])
+ test.throws(block, [error], [message])
+ test.doesNotThrow(block, [error], [message])
+ test.ifError(value)
+ */
+
+exports.util = {
+    setUp: function (done) {
+        // setup here
+        done();
+    }
+};
