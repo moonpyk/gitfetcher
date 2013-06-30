c835fead2d8611873844d7ff030bc837463edf4b
Clément Bourgeois <moonpyk@gmail.com>
2013-06-30 22:03:40 +0200

    expandUser -> expandEnv
    Added unit test.

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,6 +43,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task.
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['jshint', 'nodeunit']);
 
 };
--- test/util_test.js
+++ test/util_test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var myTest = require('../lib/util');
+var u = require('../lib/util');
 
 /*
  ======== A Handy Little Nodeunit Reference ========
@@ -26,5 +26,24 @@ exports.util = {
     setUp: function (done) {
         // setup here
         done();
+    },
+    expandEnv: function (test) {
+        process.env.HOME = "/home/foobar";
+        process.env.USERPROFILE = "C:\\Users\\foobar";
+
+        if (process.platform == "win32") {
+            test.equal(u.expandEnv("~"), process.env.USERPROFILE);
+            test.equal(u.expandEnv("${USERPROFILE}"), process.env.USERPROFILE);
+        } else {
+            test.equal(u.expandEnv("~"), "/home/foobar");
+            test.equal(u.expandEnv("${HOME}"), process.env.HOME);
+        }
+
+        test.equals(u.expandEnv("${NON_EXISTANT}"), "");
+        test.equals(u.expandEnv("${}"), "");
+        test.equals(u.expandEnv("${FOOBAR}/${ZGRUNT}"), "/");
+        test.equals(u.expandEnv("${ANOTHER_VAR}\\${ZGRUNT}"), "\\");
+
+        test.done();
     }
 };
