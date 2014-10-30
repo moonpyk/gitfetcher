b809a68b715284979bbd9b328b29bf20ebdbd872
Clément Bourgeois <moonpyk@gmail.com>
2014-10-31 00:44:25 +0100

    Progressive typescript conversion

--- lib/util.ts
+++ lib/util.ts
@@ -5,16 +5,18 @@
  * Copyright (c) 2013 Clément Bourgeois
  * Licensed under the MIT license.
  */
+
 ///<reference path="../typings/tsd.d.ts"/>
-var path = require('path');
-var _ = require('lodash');
+import path = require('path');
+import _ = require('lodash');
+
 exports = _.extend(exports, require('util'), {
     /**
      * Simple prompt
      * @param {Function} [cb]
      * @returns {*}
      */
-    prompt: function (cb) {
+    prompt: function (cb : Function) {
         return process.stdin.on('data', function (data) {
             if (_.isFunction(cb)) {
                 cb(data);
@@ -25,47 +27,63 @@ exports = _.extend(exports, require('util'), {
      * @param {String} val
      * @returns {String}
      */
-    expandEnv: function (val) {
+    expandEnv: function (val : any) {
         if (!_.isString(val)) {
             return val;
         }
+
         if (process.platform === "win32") {
             val = val.replace("~", "${USERPROFILE}");
-        }
-        else {
+
+        } else {
             val = val.replace("~", "${HOME}");
+
         }
         _(process.env).each(function (v, key) {
-            val = val.replace(new RegExp('\\$\\{' + key + '\\}', "g"), process.env[key]);
+            val = val.replace(
+                new RegExp('\\$\\{' + key + '\\}', "g"),
+                process.env[key]
+            );
         });
+
         val = val.replace(/\$\{\w*\}/g, "");
+
         return val;
     },
     /**
      * @param {String} f
      * @returns {String}
      */
-    resolveExpandEnv: function (f) {
-        return path.resolve(path.normalize(this.expandEnv(f)));
+    resolveExpandEnv: function (f : string) {
+        return path.resolve(
+            path.normalize(this.expandEnv(f))
+        );
     },
     /**
      * @param {String} path
      * @param {*} value
      */
-    dotNotationToObject: function (path, value) {
+    dotNotationToObject: function (path : string, value : any) {
         var ret = {};
+
         if (_.isString(path)) {
             var next = ret;
+
             var splt = _(path.split('.')).select(function (v) {
                 return !_.isEmpty(v);
             }).value();
+
             if (splt.length === 0) {
                 return value;
             }
+
             _(splt).each(function (v, k) {
-                next = next[v] = (splt.length - 1 === k) ? value : {};
+                next = next[v] = ( splt.length - 1 === k)
+                    ? value
+                    : {};
             });
         }
+
         return ret;
     },
     /**
@@ -74,28 +92,36 @@ exports = _.extend(exports, require('util'), {
      */
     inferString: function (val) {
         if (_.isString(val)) {
-            var asInt = parseInt(val, 0), asFloat = parseFloat(val);
+            var asInt = parseInt(val, 0),
+                asFloat = parseFloat(val);
+
             if (_.isNumber(asFloat) && !_.isNaN(asFloat)) {
                 return asFloat;
             }
+
             if (_.isNumber(asInt) && !_.isNaN(asInt)) {
                 return asInt;
             }
+
             switch (val) {
                 case "true":
                 case "yes":
                     return true;
+
                 case "false":
                 case "no":
                     return false;
+
                 case "undefined":
                     return undefined;
+
                 case "null":
                     return null;
             }
+
             return val;
         }
+
         return val;
     }
 });
-//# sourceMappingURL=util.js.map
