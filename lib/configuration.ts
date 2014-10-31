c6dec4768b3ef76007c02b9c7f9fde6a5b0f1b72
Clément Bourgeois <moonpyk@gmail.com>
2014-10-31 18:01:14 +0100

    Continued

--- lib/configuration.ts
+++ lib/configuration.ts
@@ -5,93 +5,29 @@
  * Copyright (c) 2013 Clément Bourgeois
  * Licensed under the MIT license.
  */
+
 ///<reference path="../typings/tsd.d.ts"/>
-var fs = require('fs');
-var u = require('./util');
-var nu = require('util');
+
+import fs = require('fs');
+import u = require('./util');
+import nu = require('util');
 var o = require('./output');
-var Configuration = (function () {
-    function Configuration() {
+
+class Configuration {
+    private _raw:string;
+    private _projectFilled:boolean;
+    static filename:string;
+    content:any;
+    projects:any;
+
+    constructor() {
         this._raw = "";
         this._projectFilled = false;
         this.content = {};
         this.projects = {};
     }
-    Configuration.open = function (filename) {
-        var c = new Configuration();
-        var ret = c._open(filename);
-        if (ret === false) {
-            return null;
-        }
-        Configuration.filename = filename;
-        return ret;
-    };
-    Configuration.prototype.fillProjects = function () {
-        if (this._projectFilled) {
-            return this;
-        }
-        _(this.content).each(function (v, key) {
-            if (key === "defaults") {
-                return;
-            }
-            this.projects[key] = Configuration._makeProject(this.content[key]);
-            if (_.isString(this.projects[key].path)) {
-                this.projects[key].rpath = u.resolveExpandEnv(this.projects[key].path);
-            }
-        });
-        this._projectFilled = true;
-        return this;
-    };
-    Configuration.prototype.save = function (filename) {
-        if (!_.isString(filename)) {
-            filename = Configuration.filename;
-        }
-        var jsConfig = JSON.stringify(this.content, null, o.indent(4));
-        try {
-            var os = require('os');
-            fs.writeFile(filename, jsConfig.replace(/\n/g, os.EOL) + os.EOL, 'utf8');
-        }
-        catch (ex) {
-            o.error(nu.format("Unable to save configuration to '%s'", filename));
-            return false;
-        }
-        return true;
-    };
-    Configuration.getDefaults = function () {
-        return _.clone(Configuration.defaults);
-    };
-    Configuration._makeProject = function (proj) {
-        return _.extend(Configuration.getDefaults(), proj);
-    };
-    Configuration.prototype._open = function (filename) {
-        if (_.isString(filename)) {
-            return false;
-        }
-        if (!fs.existsSync(filename)) {
-            return false;
-        }
-        try {
-            this._raw = fs.readFileSync(filename, 'utf8');
-        }
-        catch (ex) {
-            return false;
-        }
-        try {
-            this.content = JSON.parse(this._raw);
-        }
-        catch (ex) {
-            return false;
-        }
-        // Overriding hard-coded defaults, with configured ones.
-        if (_.isObject(this.content.defaults)) {
-            _.extend(Configuration.defaults, this.content.defaults);
-        }
-        else {
-            o.warning("No 'defaults' gitfetcher configuration section found. Using application code defined one.");
-        }
-        return filename;
-    };
-    Configuration.defaults = {
+
+    static defaults = {
         // Application variables
         print_git_out: true,
         exit_on_fail: false,
@@ -114,6 +50,99 @@ var Configuration = (function () {
         path: null,
         rpath: null
     };
-    return Configuration;
-})();
-//# sourceMappingURL=configuration.js.map
+
+    static open(filename:string) {
+        var c = new Configuration();
+
+        var ret = c._open(filename);
+
+        if (ret === false) {
+            return null;
+        }
+
+        Configuration.filename = filename;
+
+        return ret;
+    }
+
+    fillProjects() {
+        if (this._projectFilled) {
+            return this;
+        }
+
+        _(this.content).each(function (v, key) {
+            if (key === "defaults") {
+                return;
+            }
+
+            this.projects[key] = Configuration._makeProject(this.content[key]);
+
+            if (_.isString(this.projects[key].path)) {
+                this.projects[key].rpath = u.resolveExpandEnv(
+                    this.projects[key].path
+                );
+            }
+        });
+
+        this._projectFilled = true;
+
+        return this;
+    }
+
+    save(filename) {
+        if (!_.isString(filename)) {
+            filename = Configuration.filename;
+        }
+
+        var jsConfig = JSON.stringify(this.content, null, o.indent(4));
+
+        try {
+            var os = require('os');
+            fs.writeFile(filename, jsConfig.replace(/\n/g, os.EOL) + os.EOL, 'utf8');
+        } catch (ex) {
+            o.error(nu.format("Unable to save configuration to '%s'", filename));
+            return false;
+        }
+
+        return true;
+    }
+
+    static getDefaults () {
+        return _.clone(Configuration.defaults);
+    }
+    private static _makeProject (proj) {
+        return _.extend(Configuration.getDefaults(), proj);
+    }
+
+    private _open(filename):any {
+        if (_.isString(filename)) {
+            return false;
+        }
+
+        if (!fs.existsSync(filename)) {
+            return false;
+        }
+
+        try {
+            this._raw = fs.readFileSync(filename, 'utf8');
+        } catch (ex) {
+            return false;
+        }
+
+        try {
+            this.content = JSON.parse(this._raw);
+        } catch (ex) {
+            return false;
+        }
+
+        // Overriding hard-coded defaults, with configured ones.
+        if (_.isObject(this.content.defaults)) {
+            _.extend(Configuration.defaults, this.content.defaults);
+
+        } else {
+            o.warning("No 'defaults' gitfetcher configuration section found. Using application code defined one.");
+        }
+
+        return filename;
+    }
+}
--- lib/util.ts
+++ lib/util.ts
@@ -10,118 +10,102 @@
 import path = require('path');
 import _ = require('lodash');
 
-exports = _.extend(exports, require('util'), {
-    /**
-     * Simple prompt
-     * @param {Function} [cb]
-     * @returns {*}
-     */
-    prompt: function (cb : Function) {
-        return process.stdin.on('data', function (data) {
-            if (_.isFunction(cb)) {
-                cb(data);
-            }
-        }).resume();
-    },
-    /**
-     * @param {String} val
-     * @returns {String}
-     */
-    expandEnv: function (val : any) {
-        if (!_.isString(val)) {
-            return val;
+export function inferString(val) {
+    if (_.isString(val)) {
+        var asInt = parseInt(val, 0),
+            asFloat = parseFloat(val);
+
+        if (_.isNumber(asFloat) && !_.isNaN(asFloat)) {
+            return asFloat;
         }
 
-        if (process.platform === "win32") {
-            val = val.replace("~", "${USERPROFILE}");
-
-        } else {
-            val = val.replace("~", "${HOME}");
-
-        }
-        _(process.env).each(function (v, key) {
-            val = val.replace(
-                new RegExp('\\$\\{' + key + '\\}', "g"),
-                process.env[key]
-            );
-        });
-
-        val = val.replace(/\$\{\w*\}/g, "");
-
-        return val;
-    },
-    /**
-     * @param {String} f
-     * @returns {String}
-     */
-    resolveExpandEnv: function (f : string) {
-        return path.resolve(
-            path.normalize(this.expandEnv(f))
-        );
-    },
-    /**
-     * @param {String} path
-     * @param {*} value
-     */
-    dotNotationToObject: function (path : string, value : any) {
-        var ret = {};
-
-        if (_.isString(path)) {
-            var next = ret;
-
-            var splt = _(path.split('.')).select(function (v) {
-                return !_.isEmpty(v);
-            }).value();
-
-            if (splt.length === 0) {
-                return value;
-            }
-
-            _(splt).each(function (v, k) {
-                next = next[v] = ( splt.length - 1 === k)
-                    ? value
-                    : {};
-            });
+        if (_.isNumber(asInt) && !_.isNaN(asInt)) {
+            return asInt;
         }
 
-        return ret;
-    },
-    /**
-     * @param {String} val
-     * @return {*}
-     */
-    inferString: function (val) {
-        if (_.isString(val)) {
-            var asInt = parseInt(val, 0),
-                asFloat = parseFloat(val);
+        switch (val) {
+            case "true":
+            case "yes":
+                return true;
 
-            if (_.isNumber(asFloat) && !_.isNaN(asFloat)) {
-                return asFloat;
-            }
+            case "false":
+            case "no":
+                return false;
 
-            if (_.isNumber(asInt) && !_.isNaN(asInt)) {
-                return asInt;
-            }
+            case "undefined":
+                return undefined;
 
-            switch (val) {
-                case "true":
-                case "yes":
-                    return true;
-
-                case "false":
-                case "no":
-                    return false;
-
-                case "undefined":
-                    return undefined;
-
-                case "null":
-                    return null;
-            }
-
-            return val;
+            case "null":
+                return null;
         }
 
         return val;
     }
-});
+
+    return val;
+}
+
+export function dotNotationToObject(path:string, value:any) {
+    var ret = {};
+
+    if (_.isString(path)) {
+        var next = ret;
+
+        var splt = _(path.split('.')).select(function (v) {
+            return !_.isEmpty(v);
+        }).value();
+
+        if (splt.length === 0) {
+            return value;
+        }
+
+        _(splt).each(function (v, k) {
+            next = next[v] = ( splt.length - 1 === k)
+                ? value
+                : {};
+        });
+    }
+
+    return ret;
+}
+
+export function resolveExpandEnv(f:string) {
+    return path.resolve(
+        path.normalize(this.expandEnv(f))
+    );
+}
+
+export function expandEnv(val:any) {
+    if (!_.isString(val)) {
+        return val;
+    }
+
+    if (process.platform === "win32") {
+        val = val.replace("~", "${USERPROFILE}");
+
+    } else {
+        val = val.replace("~", "${HOME}");
+
+    }
+    _(process.env).each(function (v, key) {
+        val = val.replace(
+            new RegExp('\\$\\{' + key + '\\}', "g"),
+            process.env[key]
+        );
+    });
+
+    val = val.replace(/\$\{\w*\}/g, "");
+
+    return val;
+}
+export function prompt(cb:Function) {
+    var stdin = process.stdin;
+    stdin.on('data', function (data) {
+        if (_.isFunction(cb)) {
+            cb(data);
+        }
+    });
+
+    return stdin.resume();
+}
+
